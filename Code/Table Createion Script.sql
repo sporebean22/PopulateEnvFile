4983ac194fa99ac619951d7ecea342365bccb5a6
Dilan Ormisiar <54839418+dilansql@users.noreply.github.com>
2020-10-18 17:23:23 +0100

    file structure update

---
+++ Code/Table Createion Script.sql
@@ -0,0 +1,10 @@
+create table ENVCONFIGURATION (
+    Variable_Id int UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Project Varchar(100),
+    Variable_Name VARCHAR(MAX),
+    Variable_Value VARCHAR(MAX)
+);
+
+insert into ENVCONFIGURATION values ()
+
+SELECT * FROM sql2323722.ENVCONFIGURATION;
--- Code/WPF.ps1
+++ Code/WPF.ps1
@@ -1,4 +1,3 @@
-
 Add-Type -assemblyName PresentationFramework
 Add-Type -assemblyName PresentationCore
 Add-Type -assemblyName WindowsBase
@@ -6,17 +5,16 @@ Add-Type -assemblyName WindowsBase
 [xml]$xaml = Get-Content "WPF.xml"
 
 $currentDir = Get-Location
-$destination = Split-Path -Path $currentDir -Parent
-$assets = $destination + "\Assets"
-
 $reader = (New-Object System.Xml.XmlNodeReader $xaml)
 $window = [Windows.Markup.XamlReader]::Load($reader)
 
 $xaml.SelectNodes("//*[@*[contains(translate(name(.),'n','N'),'Name')]]") | ForEach-Object{
     Set-Variable -Name ($_.Name) -Value $window.FindName($_.Name)
-} try {
+}
+
+try {
     $window.add_Loaded({
-    $window.Icon = "$($assets.Path)\PopulateEnv.ico"})
+    $window.Icon = "$($currentDir.Path)\PopulateEnv.ico"})
 } catch {
     Write-Host "Error the Icon path is incorrect: $($currentDir.Path)\PopulateEnv.ico"
 }
